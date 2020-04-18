0b1c0c16d352b4d118b4acb76ed8b3ba5dd2f762
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-04-19 01:09:24 +0530

    Initiating project

---
+++ src/test/java/com/neeraj/nvpetclinic/NvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.nvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
