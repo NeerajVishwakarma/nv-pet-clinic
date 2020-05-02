4fa7039cad3122fc4eb169c964365a948ea4a574
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-03 01:17:36 +0530

    Refactoring the project to Multi-Module.

---
+++ nv-pet-clinic-web/src/main/java/com/neeraj/nvpetclinic/NvPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.nvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvPetClinicApplication.class, args);
+	}
+
+}
