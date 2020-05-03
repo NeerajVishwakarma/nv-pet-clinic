791ae10bf2276cb002fb8fe48eb8245070234dfd
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-04 01:53:07 +0530

    Added View and Controller for Vets

---
+++ nv-pet-clinic-web/src/main/java/com/neeraj/nvpetclinic/controller/VetController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.neeraj.nvpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Neeraj
+ *
+ */
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
+	public String listVets() {
+		return "vets/vetindex";
+	}
+}
