271e2c1e24cd68f5c50e466759438adb7a27014e
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-04 01:43:43 +0530

    Adding Index Page

---
+++ nv-pet-clinic-web/src/main/java/com/neeraj/nvpetclinic/controller/IndexController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.neeraj.nvpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Neeraj
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping({"","/","index","index.html"})
+	public String index() {
+		return "index";
+	}
+}
