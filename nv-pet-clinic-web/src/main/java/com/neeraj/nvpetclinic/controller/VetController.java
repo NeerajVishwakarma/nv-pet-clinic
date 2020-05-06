72c6c8fe3920e301c54a6fdf0f99dfaa81c63db1
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-07 01:21:50 +0530

    Listing Vets on Vet browser.

--- nv-pet-clinic-web/src/main/java/com/neeraj/nvpetclinic/controller/VetController.java
+++ nv-pet-clinic-web/src/main/java/com/neeraj/nvpetclinic/controller/VetController.java
@@ -4,7 +4,10 @@
 package com.neeraj.nvpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neeraj.nvpetclinic.services.VetService;
 
 /**
  * @author Neeraj
@@ -13,8 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/vetindex";
 	}
 }
