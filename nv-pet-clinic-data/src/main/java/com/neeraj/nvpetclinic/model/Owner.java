7fcaa4aaf6957435d925930442cda48e5a1ac216
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-11 01:05:55 +0530

    Added Visit

--- nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Owner.java
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Owner.java
@@ -3,10 +3,30 @@
  */
 package com.neeraj.nvpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Neeraj
  *
  */
 public class Owner extends Person {
 
+	private Set<Pet> pets;
+
+	/**
+	 * @return the pets
+	 */
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	/**
+	 * @param pets the pets to set
+	 */
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
+	
 }
