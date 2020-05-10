545b59dce246e48956117efa435a91fb36226373
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-11 01:09:03 +0530

    Added Speciality

---
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Speciality.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.neeraj.nvpetclinic.model;
+
+/**
+ * @author Neeraj
+ *
+ */
+public class Speciality extends BaseEntity {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
--- nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Vet.java
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Vet.java
@@ -3,10 +3,22 @@
  */
 package com.neeraj.nvpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Neeraj
  *
  */
 public class Vet extends Person {
 
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
+	
 }
