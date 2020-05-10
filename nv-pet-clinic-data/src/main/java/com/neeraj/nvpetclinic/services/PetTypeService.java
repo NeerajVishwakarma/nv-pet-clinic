98d5d5345e604d7541604db3dec82aff2cdfda3e
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-11 01:18:30 +0530

    Added PetType Map Service.

---
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.neeraj.nvpetclinic.services;
+
+import com.neeraj.nvpetclinic.model.PetType;
+
+/**
+ * @author Neeraj
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
