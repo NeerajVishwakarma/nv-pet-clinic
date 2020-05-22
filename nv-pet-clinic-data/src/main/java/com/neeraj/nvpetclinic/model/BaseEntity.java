30f062ef792e4055d45d79443cf9c860070c1c6d
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-23 02:17:19 +0530

    Updating BaseEntity and POm with hibernate.

--- nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/BaseEntity.java
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/BaseEntity.java
@@ -5,12 +5,20 @@ package com.neeraj.nvpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * @author Neeraj
  *
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
