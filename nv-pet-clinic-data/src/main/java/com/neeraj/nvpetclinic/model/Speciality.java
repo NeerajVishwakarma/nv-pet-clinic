ade9fe617a7d288b134c89edf9ee6146975af814
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-05-23 18:13:04 +0530

    Added entity of Vet and Speciality

--- nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Speciality.java
+++ nv-pet-clinic-data/src/main/java/com/neeraj/nvpetclinic/model/Speciality.java
@@ -3,12 +3,19 @@
  */
 package com.neeraj.nvpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Neeraj
  *
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
